a8e28420942c1f03388088b675c46d5bbc8ea0b8
HP5000SERIES\avila <avilameza@gmail.com>
2026-07-12 22:35:54 -0500

    se crean las clases repository tanto para postgres como para mongo

---
+++ erp-infrastructure/src/main/java/com/udemy/erp_lite/persistence/jpa/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.udemy.erp_lite.persistence.jpa.repositories;
+
+import com.udemy.erp_lite.persistence.jpa.entities.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface ProductRepository extends JpaRepository<ProductEntity, UUID> {
+}

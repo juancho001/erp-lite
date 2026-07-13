6a9119d333223080bea9aa415de3615dcc063729
HP5000SERIES\avila <avilameza@gmail.com>
2026-07-12 22:27:54 -0500

    carga de las entidades para la base de datos de mongo se uso el comando (git config --global core.longpaths true) para permitir usar archivos con nombres largos

---
+++ erp-infrastructure/src/main/java/com/udemy/erp_lite/persistence/mongo/documents/CatalogType.java
@@ -0,0 +1,9 @@
+package com.udemy.erp_lite.persistence.mongo.documents;
+
+public enum CatalogType {
+    PRODUCT_CATEGORIES,
+    ORDER_STATUSES,
+    PAYMENT_METHODS,
+    SHIPPING_METHODS,
+    COUNTRIES, CURRENCIES
+}
